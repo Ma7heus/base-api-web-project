01b16dfbd08d5a2073d08d0898527cb2709a2d37
Matheus Biasi <biasimatheushenrique40@gmail.com>
2025-11-26 01:10:10 -0300

    Adicionando suporte a migrações com TypeORM e ajustando configuração no app.module.ts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,12 +1,18 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import AppDataSource from '../database/ormconfig';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      ...AppDataSource.options,
+      autoLoadEntities: true,
     }),
   ],
   controllers: [AppController],
